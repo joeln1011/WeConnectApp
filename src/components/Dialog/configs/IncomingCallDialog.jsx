3ccc22e65be82a442d22e30d48d496d1b6b1eff5
Joel Nguyen <joelnguyen@Joels-MacBook-Pro-2.local>
2025-08-28 22:49:19 -0400

    UI Coming Call

---
+++ src/components/Dialog/configs/IncomingCallDialog.jsx
@@ -0,0 +1,42 @@
+import UserAvatar from "@components/UserAvatar";
+import { useVideoCallContext } from "@context/VideoCallProvider";
+import { Call, CallEnd } from "@mui/icons-material";
+import { Button } from "@mui/material";
+
+const IncomingCallDialog = () => {
+  const {
+    inComingCall,
+    callerInfo = {},
+    acceptCall,
+    callId,
+  } = useVideoCallContext();
+  return (
+    <div className="flex flex-col items-center p-6">
+      <UserAvatar
+        name={callerInfo?.fullName}
+        src={callerInfo?.image}
+        className="mb-3 !h-16 !w-16"
+      />
+      <p className="mb-1 text-xl font-semibold">{callerInfo?.fullName}</p>
+      <p className="text-dark-400 mb-4">Incoming Video Call...</p>
+      <div className="flex w-full justify-center gap-6">
+        <Button
+          variant="contained"
+          inputProps={{ startIcon: <Call /> }}
+          onClick={() => acceptCall(callId)}
+        >
+          Accept
+        </Button>
+        <Button
+          variant="contained"
+          inputProps={{ startIcon: <CallEnd />, color: "error" }}
+          onClick={() => acceptCall(callId)}
+        >
+          Reject
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default IncomingCallDialog;

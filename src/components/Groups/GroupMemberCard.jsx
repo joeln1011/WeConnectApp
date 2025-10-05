4348f11827ea7396e167531bf28ceb09999fcac6
Joel Nguyen <joelnguyen@Joels-MacBook-Pro-2.local>
2025-10-04 22:50:07 -0400

    Group Member Action Button

--- src/components/Groups/GroupMemberCard.jsx
+++ src/components/Groups/GroupMemberCard.jsx
@@ -1,9 +1,18 @@
 import UserAvatar from "@components/UserAvatar";
+import { useUserInfo } from "@hooks/index";
 import { Link } from "react-router-dom";
+import MemberActionButtons from "./MemberActionButtons";
 
 const GroupMemberCard = ({ id, fullName = "", avatarImage, role }) => {
+  const { _id } = useUserInfo();
+  const isMyself = _id === id;
   return (
-    <div className="card flex flex-col items-center">
+    <div className="card relative flex flex-col items-center">
+      {!isMyself && (
+        <div className="absolute top-2 right-2">
+          <MemberActionButtons />
+        </div>
+      )}
       <UserAvatar
         className="mb-3 !h-12 !w-12"
         name={fullName}
